886097fea975dbd8976c6dbc21426ea17f17457c
Adarsh Maurya <adarshmaurya7@gmail.com>
2026-06-18 14:20:45 +0530

    chore: bump Homebrew Cask to v0.1.1

--- Casks/macremote.rb
+++ Casks/macremote.rb
@@ -1,6 +1,6 @@
 cask "macremote" do
-  version "0.1.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.1.1"
+  sha256 "416775d004223a26a8097cddc86bcb54b56d223a438f8b903f047493aace83a5"
 
   url "https://github.com/adarsh9780/mac_remote/releases/download/v#{version}/MacRemote.dmg"
   name "MacRemote"
